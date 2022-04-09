180a031c1220d79b263dd722215a513970b222a0
Adrienne Matosich <amatosich@gmail.com>
2022-04-09 11:10:33 -0700

    make changes to html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   $("form#techCareer").submit(function(event){  
     event.preventDefault();
-    const techtype = $("input:radio[name=techtype]:checked").val();
+    const techType = $("input:radio[name=techType]:checked").val();
     const language = $("input:radio[name=language]:checked").val();
     const location = $("input:radio[name=location]:checked").val();
     const music = $("input:radio[name=music]:checked").val();
     const pet = $("input:radio[name=pet]:checked").val();
     
-    
-    $("#output").text(result);
+    if(techType === language && language === location) {
+      $(".match").text(techType);
+      $("result").show();
+    } else if (language === location && location === music) {
+      $(".match").text(language);
+      $("result").show();
+    } else if (location === music && location === pet) {
+      $(".match").text(location);
+    } else {
+      $(".match").text("python");
+      $("result").show();
+    }
+
+      if (techType === "Ruby") {
+        $("#rubyResult").show();
+      }
     });
   });
