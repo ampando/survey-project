8b669177aaaa436f88e26bc21524be8d0dd62cd4
Adrienne Matosich <amatosich@gmail.com>
2022-04-09 23:06:42 -0700

    update html and scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
   $("#techCareer").submit(function(event) {  
     event.preventDefault();
-    const techType = $ ("input:radio[name=techType]:checked").val();
-    const language = $ ("input:radio[name=language]:checked").val();
-    const location = $ ("input:radio[name=location]:checked").val();
-    const music = $ ("input:radio[name=music]:checked").val();
-    const pet = $ ("input:radio[name=pet]:checked").val();
+    const techType = $("input:radio[name=techType]:checked").val();
+    const language = $("input:radio[name=language]:checked").val();
+    const location = $("input:radio[name=location]:checked").val();
+    const music = $("input:radio[name=music]:checked").val();
+    const pet = $("input:radio[name=pet]:checked").val();
     
     if(techType === language && language === location) {
       $(".match").text(techType);
-      $("result").show();
+      $("#result").show();
     } else if (language === location && location === music) {
       $(".match").text(language);
-      $("result").show();
+      $("#result").show();
     } else if (location === music && location === pet) {
       $(".match").text(location);
       $("#result").show();
     } else {
-      $(".match").text("python");
-      $("result").show();
+      $(".match").text("Python");
+      $("#result").show();
     }
 
       if (techType === "hackerGenius") {
         $("#rubyResult").show();
+    }
+
+      // if (techType === "hackerGenius") {
+      //   $("#rubyResult").show();
+      // }  
       }
     });
   });
